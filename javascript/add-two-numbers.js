e3755bd16cb77c6b51bd4a956d35f9e83c30b017
hvkcoder <hvkcoder@163.com>
2018-01-16 16:32:11 +0800

    Add javascript add-two-numbers.js

--- javascript/add-two-numbers.js
+++ javascript/add-two-numbers.js
@@ -1,23 +1,45 @@
-(function(){
+(function () {
     'use strict';
-    /**
-     * addTwoNumbers function
-     * @param {Array} l1 
-     * @param {Array} l2 
-     * @return {Array}
-     */
-    function addTwoNumbers(l1, l2) {
-        // Determine whether the parameter l1,l2 is an array
-        if(!(l1 instanceof Array) && !(l2 instanceof Array)) return [];
-        // Determine whether the parameter l1,l2 there are non-numerical
-        for(let i = 0; i < l1.length; i++ ){
-            if(isNaN(l1[i]) || isNaN(l2[i])){
-                console.log('1111');
-                return [];
-            }
-        }
-        let result = parseInt(l1.join('')) + parseInt(l2.join(''));
-        return result.toString().split('').reverse();
+    //  Definition for singly-linked list.
+    function ListNode(val) {
+        this.val = val;
+        this.next = null;
     };
-    console.log(addTwoNumbers([2,4,3], [5,6,4]));
+
+    /**
+     * @param {ListNode} l1
+     * @param {ListNode} l2
+     * @return {ListNode}
+     */
+    var addTwoNumbers = function (l1, l2) {
+        let result = new ListNode(0);
+        let carry = 0;
+        let tempNode = null;
+        while (l1 || l2 || carry) {
+            // Determine whether the parameter l1 is null and l1.val value is non-numeric or null
+            let val1 = l1 && !isNaN(l1.val) && l1.val ? l1.val : 0;
+            // Determine whether the parameter l2 is null and l2.val value is non-numeric or null
+            let val2 = l2 && !isNaN(l2.val) && l2.val ? l2.val : 0;
+            // Calculate the current value
+            let currVal = Math.floor((val1 + val2 + carry) % 10);
+            // The carry value of the current value
+            carry = Math.floor((val1 + val2 + carry) / 10);
+            if(tempNode == null){
+                tempNode = new ListNode(currVal);
+                result = tempNode;
+            }else{
+                tempNode.next = new ListNode(currVal);
+                tempNode = tempNode.next;
+            }
+            if (l1) l1 = l1.next;
+            if (l2) l2 = l2.next;
+        }
+        return result;
+    };
+
+    let l1 = new ListNode(5);
+    l1.next = new ListNode(1);
+    l1.next.next = new ListNode(6);
+    let l2 = new ListNode(5);
+    console.log(addTwoNumbers(l1, l2));
 })();
