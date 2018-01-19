549e55c19780b7bf229084c7c63732c81457a3b6
hvkcoder <hvkcoder@163.com>
2018-01-19 12:04:53 +0800

    Update javascript length-of-longest-substring

---
+++ javascript/length-of-longest-substring.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    var lengthOfLongestSubstring = function (s) {
+        let charArr = [];
+        for (let i = 0; i < s.length; i++) {
+            let char = s.charAt(i);
+        }
+        return charArr.length;
+    };
+    let result = lengthOfLongestSubstring("au");
+    // let result = lengthOfLongestSubstring("abcabcbbbb");
+    // console.log(result);
+})();
