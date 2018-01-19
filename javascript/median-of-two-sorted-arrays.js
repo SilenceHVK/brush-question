372ae8b09cb0b81b169aeb083ac498bc8a78878d
hvkcoder <hvkcoder@163.com>
2018-01-19 15:54:48 +0800

    Update javascript median-of-two-sorted-arrays

---
+++ javascript/median-of-two-sorted-arrays.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number}
+     */
+    var findMedianSortedArrays = function (nums1, nums2) {
+        
+        // console.log(nums1.concat(nums2).sort());
+    };
+    findMedianSortedArrays([1, 3], [2])
+})();
