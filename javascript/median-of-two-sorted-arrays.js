0e67a54d91d1b9a4340b768d16fcc291a96367fc
hvkcoder <hvkcoder@163.com>
2018-01-23 11:02:08 +0800

    Update median-of-two-sorted-arrays.js

--- javascript/median-of-two-sorted-arrays.js
+++ javascript/median-of-two-sorted-arrays.js
@@ -6,8 +6,32 @@
      * @return {number}
      */
     var findMedianSortedArrays = function (nums1, nums2) {
-        
-        // console.log(nums1.concat(nums2).sort());
+        // Determine whether the parameter nums1 or nums2 is an array.
+        if (!(nums1 instanceof Array) || !(nums2 instanceof Array)) return 0;
+        // Merge two arrays.
+        let nums = nums1.concat(nums2);
+        // Sort the array.
+        nums.sort((a, b) => {
+            if (a < b) {
+                return - 1;
+            } else if (a > b) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        // According to the length of the array to take the median.
+        if (nums.length % 2 == 0) {
+            let index = Math.floor(nums.length / 2);
+            let prevIndex = index - 1;
+            return (nums[prevIndex] + nums[index]) / 2;
+        } else {
+            let index = Math.floor(nums.length / 2);
+            return nums[index];
+        }
     };
-    findMedianSortedArrays([1, 3], [2])
+    let result = findMedianSortedArrays([], [20, 21, 23, 23, 25, 29, 32, 33]);
+    // let result = findMedianSortedArrays([], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    // let result = findMedianSortedArrays([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22], [0, 6]);
+    console.log(result);
 })();
