62681ece226faf1e517b26d7265d070da7a25532
hvkcoder <hvkcoder@163.com>
2018-01-24 14:43:22 +0800

    Add C# twoSum.cs

--- csharp/TwoSum.cs
+++ csharp/TwoSum.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        
+        var result = new int[2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            int index = Array.IndexOf(nums, complement);
+            if (index > -1 && index != i)
+            {
+                result[0] = i;
+                result[1] = index;
+                break;
+            }
+        }
+        return result;
     }
 }
