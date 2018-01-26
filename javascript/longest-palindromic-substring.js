de1350f0f29d84b51e93fb187c059d3e90b00924
hvkcoder <hvkcoder@163.com>
2018-01-26 17:22:32 +0800

    Update longest-palindromic-substring

--- javascript/longest-palindromic-substring.js
+++ javascript/longest-palindromic-substring.js
@@ -5,9 +5,41 @@
      * @return {string}
      */
     var longestPalindrome = function (s) {
-        console.log(s);
+
+        let isPaligndromeS = function (tempString) {
+            let reverseS = tempString.split('').reverse().join('');
+            if (tempString === reverseS) {
+                return tempString;
+            } else {
+                return '';
+            }
+        };
+
+        let palindrome = isPaligndromeS(s);
+
+        if (isPaligndromeS(s) === '') {
+            for (let i = 0; i < s.length; i++) {
+                let curChar = s.charAt(i);
+                let index = s.indexOf(curChar, i + 1);
+                if (index > -1) {
+                    let tempS = s.substring(i, index + 1);
+                    if (isPaligndromeS(tempS) !== '' && tempS.length > palindrome.length) {
+                        palindrome = tempS;
+                    }
+                    if (palindrome === '') {
+                        palindrome = s.substr(0, 1);
+                    }
+                }
+            }
+        }
+        return palindrome;
     };
 
-    let result = longestPalindrome('babad');
-    // console.log(result);
+    // let result = longestPalindrome('ccc');
+    // let result = longestPalindrome('babad');
+    // let result = longestPalindrome('cbbd');
+    // let result = longestPalindrome('');
+    // let result = longestPalindrome('a');
+    // let result = longestPalindrome("abcda");
+    console.log(result);
 })();
