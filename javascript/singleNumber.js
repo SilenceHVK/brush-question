020d73cf93354c01b7033f6819fa69861981ed76
hvkcoder <hvkcoder@163.com>
2018-06-24 14:42:00 +0800

    update javascript singleNumber

--- javascript/singleNumber.js
+++ javascript/singleNumber.js
@@ -4,10 +4,10 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
-    
+var singleNumber = function (nums) {
+    let result = 0;
+    for (let i = 0; i < nums.length; i++) {
+        result ^= nums[i];
+    }
+    return result;
 };
-
-var test = [2,2,1];
-
-console.log(test[])
