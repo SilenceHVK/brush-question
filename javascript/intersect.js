eb345dc621789a8fd5ec00b60ac2f2c06960b5de
hvkcoder <hvkcoder@163.com>
2018-06-25 10:18:20 +0800

    update javascript intersect

--- javascript/intersect.js
+++ javascript/intersect.js
@@ -7,14 +7,13 @@
  */
 var intersect = function (nums1, nums2) {
     const result = [];
+    const temp = nums2.slice(0);
     for (let i = 0; i < nums1.length; i++) {
         const item = nums1[i];
-        
-        // if (nums2.includes(item) && !(result.includes(item))) {
-        //     result.push(item);
-        // }
+        if (temp.includes(nums1[i])) {
+            result.push(item);
+            temp.splice(temp.indexOf(item), 1);
+        }
     }
     return result;
 };
-
-console.log(intersect([1, 2, 2, 1], [2, 2]));
