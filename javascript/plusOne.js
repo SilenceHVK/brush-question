5577882262778583bb966f9f837e8de281962e9b
hvkcoder <hvkcoder@163.com>
2018-06-26 11:29:51 +0800

    add javascript plusOne

--- javascript/plusOne.js
+++ javascript/plusOne.js
@@ -5,6 +5,16 @@
  * @return {number[]}
  */
 var plusOne = function (digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] === 9) {
+            digits[i] = 0;
+        } else {
+            digits[i] += 1;
+            return digits;
+        }
+    }
+    if (digits[0] === 0) {
+        digits.unshift(1);
+    }
+    return digits;
 };
-
-console.log(plusOne([1, 2, 3]));
